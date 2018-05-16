c9638bce7d13442f293fb30d0691bd5301e4fc86
Matt <matthew.meyer29@gmail.com>
2018-05-16 14:46:04 -0600

    Removed commented-out code

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,3 @@
-// Update with your config settings.
 
 module.exports = {
 
@@ -6,34 +5,8 @@ module.exports = {
     client: 'pg',
     connection: "postgres:///nyresolutions"},
 
-//   staging: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user:     'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   },
-//
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL + "?ssl=true"
   }
 };
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   }
-//
-// };
